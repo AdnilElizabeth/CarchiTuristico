25463627e01d3fe9a06a530c9ae8fe62645a6166
AdnilElizabeth <adnil.jimenez@gmail.com>
2015-09-06 21:44:53 -0500

    ++ mmapas

--- admin/constante.php
+++
@@ -1,8 +0,0 @@
-<?php
-   	define("BD","turismo");
-	define("SERVIDOR","localhost");
-	define("USUARIO","postgres");
-	define("CLAVE","root");
-	// define("CLAVE","rootdow");
-	define("PUERTO",5432);
-?>
