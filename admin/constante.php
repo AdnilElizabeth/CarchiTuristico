b30fa1f78848223dfb99d336c34a1c45299a5fe2
AdnilElizabeth <adnil.jimenez@gmail.com>
2015-09-19 21:04:04 -0500

    cambio base

--- admin/constante.php
+++ admin/constante.php
@@ -2,7 +2,7 @@
    	define("BD","turismo");
 	define("SERVIDOR","localhost");
 	define("USUARIO","postgres");
-	// define("CLAVE","root");
-	define("CLAVE","rootdow");
+	define("CLAVE","root");
+	// define("CLAVE","rootdow");
 	define("PUERTO",5432);
 ?>
